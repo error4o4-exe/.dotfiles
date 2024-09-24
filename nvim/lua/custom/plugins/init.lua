ce7759d7df838841207dcd205042f1542ae28ae9
He0x71D <mooreslaw21@gmail.com>
2024-09-24 19:27:19 +0100

    .

---
+++ nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,43 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  { 'Civitasv/cmake-tools.nvim' },
+  { 'diegoulloao/neofusion.nvim', priority = 1000, config = true, opts = ... },
+  {
+    'stevearc/quicker.nvim',
+    event = 'FileType qf',
+    ---@module "quicker"
+    ---@type quicker.SetupOptions
+    opts = {},
+  },
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+
+      -- Only one of these is needed.
+      'nvim-telescope/telescope.nvim', -- optional
+      'ibhagwan/fzf-lua', -- optional
+      'echasnovski/mini.pick', -- optional
+    },
+    config = true,
+  },
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  },
+  {
+    'aliqyan-21/wit.nvim',
+    config = function()
+      require('wit').setup()
+    end,
+  },
+}
